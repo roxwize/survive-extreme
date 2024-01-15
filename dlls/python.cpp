10a526d69c3b0562bdc87b380490d252bae3f92e
roxwize <biscordbro@gmail.com>
2024-01-15 12:14:18 -0500

    Add shootys sounds

--- dlls/python.cpp
+++ dlls/python.cpp
@@ -68,6 +68,9 @@ void CPython::Precache()
 	PRECACHE_SOUND("weapons/357_cock1.wav");
 	PRECACHE_SOUND("weapons/357_shot1.wav");
 	PRECACHE_SOUND("weapons/357_shot2.wav");
+    PRECACHE_SOUND("weapons/357_shot3.wav");
+    PRECACHE_SOUND("weapons/357_shot4.wav");
+    PRECACHE_SOUND("weapons/357_shot5.wav");
 
 	m_usFirePython = PRECACHE_EVENT(1, "events/python.sc");
 }
